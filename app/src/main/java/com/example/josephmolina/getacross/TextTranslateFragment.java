4b03d4222c34c9e8f1af0ac01985f167a56bd315
Joseph Molina <jomolina@csumb.edu>
2018-01-09 23:28:25 -0800

    Implemented text to speech functionality (closes #5)

--- app/src/main/java/com/example/josephmolina/getacross/TextTranslateFragment.java
+++ app/src/main/java/com/example/josephmolina/getacross/TextTranslateFragment.java
@@ -1,11 +1,18 @@
 package com.example.josephmolina.getacross;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
@@ -13,17 +20,35 @@ import android.view.ViewGroup;
  */
 public class TextTranslateFragment extends Fragment {
 
+    @BindView(R.id.textToTranslate)
+    EditText textToTranslate;
+    private Unbinder unbinder;
+    private TextToSpeechManager textToSpeechManager = null;
 
     public TextTranslateFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate_text, container, false);
+        View view = inflater.inflate(R.layout.fragment_translate_text, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        initializeTextToSpeechManager(view);
+        return view;
     }
 
+    @OnClick(R.id.speakButton)
+    public void onSpeak() {
+        if (!textToTranslate.getText().toString().isEmpty()) {
+            textToSpeechManager.initQueue(textToTranslate.getText().toString());
+        } else {
+            Toast.makeText(getActivity(), R.string.empty_translate_text_input, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void initializeTextToSpeechManager(View view) {
+        textToSpeechManager = new TextToSpeechManager();
+        textToSpeechManager.initialize(view.getContext());
+    }
 }
